71bbfa37bfa6c6155ac0b8d90174bbfd3cf7b6ec
Mohammad Salah <83545789+MOSALAH-CODE@users.noreply.github.com>
2023-10-20 11:11:59 +0300

    fetched data from jsonplaceholder usnig axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Row, Col } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import { ShimmerText } from "react-shimmer-effects";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/todos/"
+        );
+
+        setTodos(response.data);
+
+        setTimeout(() => {
+          setIsLoading(false); // Set isLoading to false after data is fetched
+        }, 1000);
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false); // Handle error and set isLoading to false
+      }
+    };
+    fetchTodos();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          {todos.map((todo) => (
+            <Col className="border p-3 col-md-4" key={todo.id}>
+              {isLoading ? (
+                <div className="w-100">
+                  <ShimmerText line={2} gap={10} />
+                </div>
+              ) : (
+                <>
+                  <h4>{todo.title}</h4>
+                  <p>Completed: {todo.completed ? "Yes" : "No"}</p>
+                </>
+              )}
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }
